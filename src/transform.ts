55c6bfb954eb25ae17c7507c1b4697f7fcc18d15
markthree <1801982702@qq.com>
2022-05-07 16:40:45 +0800

    feat: add basic transform

---
+++ src/transform.ts
@@ -0,0 +1,35 @@
+import $ from 'gogocode'
+
+type Include = 'all' | (keyof typeof console)[]
+
+/**
+ * transform code
+ * @param code the code of source
+ * @param include scope to be removed
+ * @returns the code of transformed
+ */
+export const transform = (
+	code: string,
+	include: Include = ['log', 'warn', 'error']
+) => {
+	let selectors
+	if (include === 'all') {
+		selectors = `console.$_$()`
+	} else if (Array.isArray(include)) {
+		selectors = include.map(type => {
+			return `console.${type}()`
+		})
+	} else {
+		throw new Error(
+			`transform's include should be string or string[]`
+		)
+	}
+
+	return (
+		$(code)
+			// @ts-ignore
+			.find(selectors)
+			.remove()
+			.generate()
+	)
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,56 @@
+import { transform } from '../src/transform'
 import { describe, expect, it } from 'vitest'
 
-describe('init', () => {
-	it('init test', () => {
-		expect(1).toBe(1)
+describe('remove log', () => {
+	it('basic', () => {
+		expect(
+			transform(`console.log("foo")`)
+		).toMatchInlineSnapshot('""')
+	})
+
+	it('multi parameter', () => {
+		expect(
+			transform(`console.log("foo", "bar", "jack");`)
+		).toMatchInlineSnapshot('""')
+	})
+
+	it('other types', () => {
+		expect(
+			transform(`console.warn("foo"); console.error("bar")`)
+		).toMatchInlineSnapshot('""')
+	})
+
+	it('other types', () => {
+		expect(
+			transform(`console.warn("foo"); console.error("bar")`)
+		).toMatchInlineSnapshot('""')
+	})
+
+	it('include', () => {
+		expect(
+			transform(
+				`console.log('log'); console.warn("warn"); console.error("error")`,
+				['log', 'error']
+			)
+		).toMatchInlineSnapshot('"console.warn(\\"warn\\");"')
+
+		expect(
+			transform(
+				`console.clear();console.error("bar")`,
+				'all'
+			)
+		).toMatchInlineSnapshot('""')
+	})
+
+	it('typecheck', () => {
+		expect(() =>
+			transform(
+				`console.clear();`,
+				// @ts-ignore
+				false
+			)
+		).toThrow(
+			`transform's include should be string or string[]`
+		)
 	})
 })
