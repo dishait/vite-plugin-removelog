9c5d6a6287c52eb8f0169b1850ac22c02e399c58
markthree <1801982702@qq.com>
2022-05-15 19:00:45 +0800

    test: add compress

--- examples/vite/vite.config.ts
+++ examples/vite/vite.config.ts
@@ -3,7 +3,14 @@ import Vue from '@vitejs/plugin-vue'
 import Jsx from '@vitejs/plugin-vue-jsx'
 import Inspect from 'vite-plugin-inspect'
 import Removelog from 'vite-plugin-removelog'
+import Compress from 'vite-plugin-compression'
 
 export default defineConfig({
-	plugins: [Vue(), Inspect(), Jsx(), Removelog()]
+	plugins: [
+		Vue(),
+		Jsx(),
+		Inspect(),
+		Compress(),
+		Removelog()
+	]
 })
