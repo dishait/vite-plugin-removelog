286c9f4c3cbf967fdbb5985eab19fe22708e2c7f
markthree <1801982702@qq.com>
2022-12-08 13:13:15 +0800

    chore: update deps

--- examples/vite/vite.config.ts
+++ examples/vite/vite.config.ts
@@ -3,14 +3,12 @@ import Vue from '@vitejs/plugin-vue'
 import Jsx from '@vitejs/plugin-vue-jsx'
 import Inspect from 'vite-plugin-inspect'
 import Removelog from 'vite-plugin-removelog'
-import Compress from 'vite-plugin-compression'
 
 export default defineConfig({
 	plugins: [
 		Vue(),
 		Jsx(),
 		Inspect(),
-		Compress(),
 		Removelog()
 	]
 })
